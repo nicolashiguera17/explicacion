be0a372352fc9f22e5a62aee08b764da08d2aea0
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2024-12-04 07:10:52 -0500

    operadores aritmeticos y de comparacion uso del if, else

---
+++ main.py
@@ -0,0 +1,28 @@
+# Libreria matematica 
+# Import math
+
+# operadores arigmeticos ' Number '
+# + - * / // ** %
+
+# Operador arigmetico Abreviado
+#numero1= 5
+#numero2 = 5
+
+#resultado = numero1 + numero2
+
+#Operadores de comparacion
+#> == != < >= <= !==
+
+print(" Condigo Inicio")
+edad= int(print("Ingrese su edad:"))
+
+if (edad <= 5):
+    print("Usted es un bebe")
+else:
+    print("Usted es un .....")
+if (edad>= 12):
+    print("Usted es un niñ@")
+else:
+    print("Usted es un ....")
+
+print("Codigo terminado")
